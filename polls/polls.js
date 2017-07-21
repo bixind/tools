5d40cd1639be183f97927722ae315d69edbcbd52
bixind <vbif-98@mail.ru>
2017-07-21 14:00:21 +0400

    prefinal

--- polls/polls.js
+++ polls/polls.js
@@ -1,26 +1,77 @@
 $(function() {
     var session;
+    function show_error(name) {
+        $('#errors').text(name);
+        $('#errors').removeClass("hidden");
+    };
+    function hide_error() {
+            $('#errors').addClass("hidden");
+    }
     $('#login').click(function(){
         VK.Auth.login(function(response) {
             if (response.session) {
-                $('#login-failed').addClass("hidden");
+                hide_error();
                 $('#polls-body').removeClass("hidden");
                 session = response.session;
                 VK.Api.call('users.get', {user_ids: response.session.user.id}, function(r) {
                     console.log(r);
                 });
             } else {
-                $('#login-failed').removeClass("hidden");
+                show_error('Не удалось подключить профиль');
             }
+        }, 8192);
+    });
+
+    function addPoll(place, owner_id, poll_id) {
+        VK.Api.call('polls.getById', {owner_id: owner_id, poll_id: poll_id: }, function(r) {
+            var poll = r.response;
+            var part = '<div class="panel panel-default">\
+                            <div class="panel-heading">' + poll.question + '</div>\
+                            <table class="table">\
+                                <thead>\
+                                <tr>\
+                                    <th>Вариант</th>\
+                                    <th>Голосов</th>\
+                                    <th>Процент</th>\
+                                </tr>\
+                                </thead>\
+                                <tbody>';
+
+            poll.answers.forEach(function(item){
+                part = part + '<tr>' +
+                        '<th>' + item.text + '</th>' +
+                        '<th>' + item.votes + '</th>' +
+                        '<th>' + item.rate + '</th>' +
+                        '</tr>';
+            });
+            part = part +
+            '</tbody>\
+                </table>\
+                <div class="panel-body">\
+                Голосов:' + poll.votes +
+                '</div>\
+            </div>';
+            place.append(part);
         });
-    }, 8192);
-    var val = 0;
-    $("#testb").click(function() {
+    };
+
+    var re = /-?\d+_\d+$/;
+    $("#url-button").click(function() {
+        var s = $('#tg-url').val();
+        var res = re.exec(s);
+        if (!res) {
+            show_error("Неверный url");
+            return;
+        }
+        hide_error();
         var place = $('#polls-list');
         place.empty();
-        for (i = 0; i < 3; ++i) {
-            place.append('<p>' + val + '</p>');
-            val = val + 1;
-        }
+        VK.Api.call('wall.getById', {posts: res[0] }, function(r) {
+            r.response[0].attachments.forEach(function(item){
+                if (item.type === 'poll') {
+                    addPoll(place, r.response[0].from_id, item.poll.poll_id);
+                }
+            });
+        });
     });
 });
