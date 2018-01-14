0090b3f192db3660ed2aa4421a7622925c29ce23
bixind <vbif-98@mail.ru>
2018-01-14 14:25:15 +0300

    test

--- activity/activity.js
+++ activity/activity.js
@@ -1,7 +1,7 @@
 $(function () {
-    $('#login').click(loginFunc(8192 | 4096));
+    $('#login').click(loginFunc(2 | 4096));
     $('#listen-button').click(function () {
-        VK.API.call('messages.getLongPollServer', {need_pts: 0, lp_version: 2}, function (r) {
+        VK.Api.call('messages.getLongPollServer', {need_pts: 0, lp_version: 2}, function (r) {
             console.log(r);
         });
     });
