d84bf9502b62e63d78cd5c42649c860c5748932e
bixind <vbif-98@mail.ru>
2018-01-14 15:18:52 +0300

    test

--- activity/activity.js
+++ activity/activity.js
@@ -1,5 +1,14 @@
 $(function () {
-    $('#login').click(loginFunc(2 | 4096));
+    var scope = 2 | 4096;
+    $('#login').click(function () {
+        var params = {client_id: 6121199,
+            redirect_uri: "https://bixind.github.io/tools/activity/index.html",
+            scope: scope,
+            response_type: 'token',
+            v: 5.69
+        };
+        window.open("https://oauth.vk.com/authorize?" + $.param(params),"_self");
+    });
     $('#listen-button').click(function () {
         VK.Api.call('messages.getLongPollServer', {need_pts: 0, lp_version: 2}, function (r) {
             console.log(r);
